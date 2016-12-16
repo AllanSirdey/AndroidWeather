4802baae9e1b39c416111a26b772019b4a0394be
Julienh56 <haasjulien@gmail.com>
2016-12-16 16:19:17 +0100

    commit

--- app/src/main/java/com/example/allan/androidweather/MainActivity.java
+++ app/src/main/java/com/example/allan/androidweather/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ok commentaire ok
         // allan
+        // fonctionnalité julien
     }
 }
